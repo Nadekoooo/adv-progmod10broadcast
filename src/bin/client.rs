bc992ee62a3cef4c913eba1ee0fda8a4f94ab7ae
Nadekoooo <christianyudistira09@gmail.com>
2025-05-23 12:45:35 +0700

    Experiment 2.1: Original code, and howit run

---
+++ src/bin/client.rs
@@ -0,0 +1,63 @@
+use futures_util::stream::StreamExt;
+use futures_util::SinkExt;
+use http::Uri;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), tokio_websockets::Error> {
+    let (mut ws_stream, _) =
+        ClientBuilder::from_uri(Uri::from_static("ws://127.0.0.1:2000"))
+            .connect()
+            .await?;
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+    let (mut sender, mut receiver) = ws_stream.split();
+
+    let stdin_to_ws = tokio::spawn(async move {
+        println!("Type a message and press Enter to send:");
+        
+        while let Ok(Some(line)) = stdin.next_line().await {
+            if line.is_empty() {
+                continue;
+            }
+            
+            if let Err(e) = sender.send(Message::text(line)).await {
+                eprintln!("Error sending message: {}", e);
+                break;
+            }
+        }
+        
+        Ok::<_, tokio_websockets::Error>(())
+    });
+
+    let ws_to_stdout = tokio::spawn(async move {
+        while let Some(message) = receiver.next().await {
+            match message {
+                Ok(msg) => {
+                    if msg.is_text() {
+                        println!("Received: {}", msg.as_text().unwrap_or(""));
+                    } else if msg.is_close() {
+                        println!("Server closed connection");
+                        break;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error receiving message: {}", e);
+                    break;
+                }
+            }
+        }
+        
+        Ok::<_, tokio_websockets::Error>(())
+    });
+
+    tokio::select! {
+        _ = stdin_to_ws => println!("Stdin task completed"),
+        _ = ws_to_stdout => println!("WebSocket receive task completed"),
+    }
+
+    Ok(())
+}
